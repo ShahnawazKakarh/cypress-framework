687f4349d98be7f896d3a0903a4560321b5ab7c8
shahnawazkakarh <shahnawaz.jrw@gmail.com>
2025-05-20 02:59:18 +0500

    Finish contact us feature

--- cypress/e2e/sk-testing/contact_us.js
+++ cypress/e2e/sk-testing/contact_us.js
@@ -3,10 +3,29 @@
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         //cypress code
-        cy.click()
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        // class with dot
+        // hash # with id
+        // square brackets [] for any other div element
+        cy.get('[name="first_name"]').type("Joe");
+        cy.get('[name="last_name"]').type("Bidden");
+        cy.get('[name="email"]').type("xyz@gmail.com");
+        cy.get('textarea.feedback-input').type("This is a feedback section");
+        cy.get('[type="submit"]').click();
+        
     });
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         //cypress code
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("Donald");
+        cy.get('[name="last_name"]').type("Trump");
+        cy.get('textarea.feedback-input').type("This is a feedback section");
+        cy.get('[type="submit"]').click();
+        cy.get('body')
+            // This assertion helps avoid flakiness in your test scripts by ensuring that the element is present in the DOM
+          .should('be.visible') // Check if the body element is visible in DOM
+          .and('contain.text', '\n\n\n Error: all fields are required\n Error: Invalid email address\n\n\n'); // Check if it contains the expected bank name
+        
     });
 })
